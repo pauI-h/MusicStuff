c4c6b42fb287c89ca16421ac57a89860e1d248ed
pauI-h <01pholland@gmail.com>
2023-05-25 14:34:28 +0100

    Initial commit

---
+++ Shifter.py
@@ -0,0 +1,132 @@
+import sys
+import time
+
+import pyautogui
+
+OS = "APP"
+if sys.platform == "win32":
+    from win32gui import GetForegroundWindow, GetWindowText
+
+    OS = "WIN"
+
+
+    def getName():
+        return GetWindowText(GetForegroundWindow())
+else:
+    from AppKit import NSWorkspace
+
+
+    def getName():
+        return NSWorkspace.sharedWorkspace().activeApplication()
+
+
+def gotoTab(name: str):
+    window = getName()
+    count = 0
+    while name not in str(window):
+        count += 1
+        if OS == "WIN":
+            pyautogui.keyDown("alt")
+        else:
+            pyautogui.keyDown("command")
+
+        for i in range(count):
+            pyautogui.press("tab")
+
+        if OS == "WIN":
+            pyautogui.keyUp("alt")
+        else:
+            pyautogui.keyUp("command")
+
+        window = getName()
+        print(getName())
+        if count > 10:
+            raise Exception("Could not find tab")
+        # time.sleep()
+
+
+def selectRow(row_num: int):
+    pyautogui.press("left")
+    if OS == "WIN":
+        pyautogui.hotkey("ctrl", "home")  # command fn left
+    else:
+        pyautogui.hotkey("command", "fn", "left")
+    pyautogui.press("right")
+
+    if OS == "WIN":
+        pyautogui.keyDown("alt")  # option
+    else:
+        pyautogui.keyDown("option")
+
+    for i in range(row_num):
+        print(i)
+        pyautogui.press("down")
+
+    if OS == "WIN":
+        pyautogui.keyUp("alt")  # option
+    else:
+        pyautogui.keyUp("option")
+
+    pyautogui.hotkey("ctrl", "shift", "end")  # shift command fn right
+
+
+def shiftRow(shift: int):
+    for i in range(shift):
+        pyautogui.press("down")
+
+
+def export(row: int, window_name: str, file_name: str):
+    pyautogui.press("left")
+    pyautogui.moveTo(1, 1)
+    pyautogui.click()
+    time.sleep(0.5)
+    press = 11
+    if OS != "WIN":
+        press += 1
+    for i in range(press):
+        pyautogui.press("down")
+    time.sleep(1)
+    pyautogui.press("enter")
+
+    if OS == "APP" and window_name not in getName():
+        gotoTab(window_name)
+        export(row, window_name, file_name)
+        return
+
+    if OS == "WIN":
+        if "Log in" in getName():
+            pyautogui.press("esc")
+            export(row, window_name, file_name)
+            return
+
+        if "Export" not in getName():
+            gotoTab(window_name)
+            export(row, window_name, file_name)
+            return
+
+    time.sleep(0.5)
+    pyautogui.press("enter")
+    for i in range(row + 1):
+        pyautogui.press("down")
+
+    pyautogui.press("enter")
+
+    pyautogui.press("tab")
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+
+    for i in range(3):
+        pyautogui.press("down")
+    pyautogui.press("enter")
+
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+
+    pyautogui.write(file_name)
+    pyautogui.press("enter")
+
+
+gotoTab("Get")
+selectRow(3)
+shiftRow(2)
+export(3, "Get", "Get Lucky Lower Bass")
